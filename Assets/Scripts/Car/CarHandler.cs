57e930e894ac02a3a8b22f2214f1a7ddbce9afa1
3nethz <ggthineth@gmail.com>
2026-04-13 16:05:44 +0530

    feat: add car steering and speed limits

--- Assets/Scripts/Car/CarHandler.cs
+++ Assets/Scripts/Car/CarHandler.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarHandler: MonoBehaviour
+public class CarHandler : MonoBehaviour
 {
     [SerializeField]
     Rigidbody rb;
+
+    [SerializeField]
+    Transform gameModel;
+
+    //Max Values
+    float maxSteerVelocity = 2;
+    float maxForwardVelocity = 30;
 
     //Multipliers
     float accelerationMultiplier = 3;
@@ -14,7 +21,18 @@ public class CarHandler: MonoBehaviour
 
     //Input
     Vector2 input = Vector2.zero;
-    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //rotate car model when turning
+        gameModel.transform.rotation = Quaternion.Euler(0, rb.linearVelocity.x * 5, 0);
+    }
+
     private void FixedUpdate()
     {
         if (input.y > 0)
@@ -27,12 +45,19 @@ public class CarHandler: MonoBehaviour
             Brake();
 
         Steer();
+
+        //Force the car not to go backwards
+        if (rb.linearVelocity.z <= 0)
+            rb.linearVelocity = Vector3.zero;
     }
 
     void Accelerate()
     {
-        rb.linearDamping = 0;
+        rb.linearDamping = 0.2f;
 
+        //Stay within the speed limit
+        if (rb.linearVelocity.z >= maxForwardVelocity)
+            return;
         rb.AddForce(rb.transform.forward * accelerationMultiplier * input.y);
     }
 
@@ -49,7 +74,30 @@ public class CarHandler: MonoBehaviour
     {
         if (Mathf.Abs(input.x) > 0)
         {
-            rb.AddForce(rb.transform.right * steerMultiplier * input.x);
+            //Move the car sideways
+            float speedBasedSteerLimit = rb.linearVelocity.z / 5.0f;
+            speedBasedSteerLimit = Mathf.Clamp01(speedBasedSteerLimit);
+
+            rb.AddForce(rb.transform.right * steerMultiplier * input.x * speedBasedSteerLimit);
+
+            //Normalize the x velocity
+            float normalizedX = rb.linearVelocity.x / maxSteerVelocity;
+
+            //Ensure it does not get bigger than 1 in magnitude
+            normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
+
+            //Make sure we stay within the turn speed limit
+            rb.linearVelocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.linearVelocity.z);
+            // Debug.Log(rb.linearVelocity.x);
+        }
+        else
+        {
+            //Auto center car
+            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, new Vector3(0, 0, rb.linearVelocity.z), Time.fixedDeltaTime * 3);
+            Vector3 pos = rb.position;
+            pos.x = Mathf.Lerp(pos.x, 0, Time.fixedDeltaTime);
+            rb.MovePosition(pos);
+            Debug.Log("X velocity: " + rb.linearVelocity.x);
         }
     }
 
